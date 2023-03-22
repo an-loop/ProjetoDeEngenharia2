ec00dec6482cbfcc80a87dd58cd4f591aca358a4
Livia Stefane Hipólito Pires <liviahipolitoo@gmail.com>
2023-03-21 21:46:26 -0300

    quarta contribuição

--- atividade_1.c
+++ atividade_1.c
@@ -5,5 +5,6 @@ int main(){
     printf("Amanda Lopes, 202207040043");
 	printf("Lohany Silva, 202207040046");
 	printf("Adryele Costa, 202206840035");
+	printf("Livia Hipolito, 202206840043");
 	return 0;
 }
