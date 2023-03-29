19b738d76df7621a397734d048003c44a0794f0e
an-loop <107003398+an-loop@users.noreply.github.com>
2023-03-29 10:29:22 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -2,9 +2,9 @@
 
 int main(){
     printf("Ola!\n");
-    printf("Amanda Lopes, 202207040043");
-	printf("Lohany Silva, 202207040046");
-	printf("Adryele Costa, 202206840035");
+    printf("Amanda Lopes, 202207040043\n");
+	printf("Lohany Silva, 202207040046\n");
+	printf("Adryele Costa, 202206840035\n");
 	printf("Livia Hipolito, 202206840043");
 	return 0;
 }
